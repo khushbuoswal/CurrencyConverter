f1ff97f9bfcc03827a0e56cd4a39f9caf22c9416
khushbuoswal <khushbuoswal13@gmail.com>
2025-05-15 18:59:39 +1000

    Add updated api link

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".convert");
+const msg = document.querySelector(".msg");
 
 for(let select of dropdowns){
     for(CurrencyCode in countryList){
@@ -34,11 +35,10 @@ const updateExchangeRate = async () => {
         amtVal = 1;
         amount.value = 1;
     }
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-    let response = await fetch(url);
+    const URL = `${BASE_URL}/currencies/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
     let data = await response.json();
-    let rate = data[toCurr.value.toLowerCase()];
-
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
     let finalAmount = amtVal * rate;
     msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
 }
